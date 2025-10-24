2fee87af3ff0dffc5508df1a06a29f8605f563f5
yuliaborodich <borodichyulia18@gmail.com>
2025-10-24 15:11:20 +0300

    feat: added swagger

---
+++ app/Http/Controllers/Swagger/TaskController.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Http\Controllers\Swagger;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Post(
+ *     path="/api/tasks",
+ *     summary="Adds a new task",
+ *     tags={"Task"},
+ *
+ *     @OA\RequestBody(
+ *         @OA\JsonContent(
+ *             allOf={
+ *                 @OA\Schema(
+ *                     @OA\Property(property="title", type="string", example="Task name"),
+ *                     @OA\Property(property="description", type="string", example="Task description"),
+ *                     @OA\Property(property="status", type="string", enum={"pending", "in_progress", "done"}, example="pending"),
+ *                 )
+ *             }
+ *         )
+ *     ),
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="id", type="integer", example=1),
+ *             @OA\Property(property="description", type="string", example="Task description"),
+ *             @OA\Property(property="status", type="string", enum={"pending", "in_progress", "done"}, example="pending"),
+ *             @OA\Property(property="created_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *             @OA\Property(property="updated_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *         )
+ *     )
+ * ),
+ *
+ * @OA\Get(
+ *    path="/api/tasks",
+ *    summary="Get all tasks",
+ *    tags={"Task"},
+ *
+ *    @OA\Response(
+ *        response=200,
+ *        description="OK",
+ *        @OA\JsonContent(
+ *            @OA\Property(property="data", type="array",
+ *                @OA\Items(
+ *                    @OA\Property(property="id", type="integer", example=1),
+ *                    @OA\Property(property="description", type="string", example="Task description"),
+ *                    @OA\Property(property="status", type="string", enum={"pending", "in_progress", "done"}, example="pending"),
+ *                    @OA\Property(property="created_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *                    @OA\Property(property="updated_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *                )
+ *            )
+ *        )
+ *    )
+ *),
+ *
+ * @OA\Get(
+ *     path="/api/tasks/{task}",
+ *     summary="Get task by id",
+ *     tags={"Task"},
+ *
+ *     @OA\Parameter(
+ *         description="Task id",
+ *         in="path",
+ *         name="task",
+ *         required=true,
+ *         example=1,
+ *     ),
+ *
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK",
+ *         @OA\JsonContent(
+ *             @OA\Property(property="data", type="object",
+ *                 @OA\Property(property="id", type="integer", example=1),
+ *                 @OA\Property(property="description", type="string", example="Task description"),
+ *                 @OA\Property(property="status", type="string", enum={"pending", "in_progress", "done"}, example="pending"),
+ *                 @OA\Property(property="created_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *                 @OA\Property(property="updated_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *             )
+ *         )
+ *     )
+ * ),
+ *
+ * @OA\Put(
+ *      path="/api/tasks/{task}",
+ *      summary="Update task by id",
+ *      tags={"Task"},
+ *
+ *      @OA\Parameter(
+ *          description="Task id",
+ *          in="path",
+ *          name="task",
+ *          required=true,
+ *          example=2,
+ *      ),
+ *
+ *     @OA\RequestBody(
+ *          @OA\JsonContent(
+ *              allOf={
+ *                  @OA\Schema(
+ *                      @OA\Property(property="title", type="string", example="Task name"),
+ *                      @OA\Property(property="description", type="string", example="Task description"),
+ *                      @OA\Property(property="status", type="string", enum={"pending", "in_progress", "done"}, example="pending"),
+ *                  )
+ *              }
+ *          )
+ *      ),
+ *
+ *      @OA\Response(
+ *          response=200,
+ *          description="OK",
+ *          @OA\JsonContent(
+ *              @OA\Property(property="data", type="object",
+ *                  @OA\Property(property="id", type="integer", example=1),
+ *                  @OA\Property(property="description", type="string", example="Task description"),
+ *                  @OA\Property(property="status", type="string", enum={"pending", "in_progress", "done"}, example="pending"),
+ *                  @OA\Property(property="created_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *                  @OA\Property(property="updated_at", type="string", example="2025-10-24T08:46:16.000000Z"),
+ *              )
+ *          )
+ *      )
+ *  ),
+ *
+ * @OA\Delete(
+ *     path="/api/tasks/{task}",
+ *     summary="Delete task by id",
+ *     tags={"Task"},
+ *     @OA\Parameter(
+ *         description="Task id",
+ *         in="path",
+ *         name="task",
+ *         required=true,
+ *         example=3,
+ *     ),
+ *
+ *     @OA\Response(
+ *         response=204,
+ *         description="No Content",
+ *     )
+ * ),
+ */
+class TaskController extends Controller
+{
+    //
+}
---
+++ app/Http/Swagger/swagger.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="Простое API для демонстрации Swagger",
+ *     @OA\Contact(
+ *         email="admin@example.com"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000/api",
+ *     description="Основной сервер"
+ * )
+ *
+ * @OA\PathItem(
+ *     path="/api"
+ * )
+ */
